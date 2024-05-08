2b0bcbcd4f6dd6e2b0e43f6d4216e596ce7853ad
tiffany <tiffnyalexa3@gmailc.om>
2024-05-08 18:32:12 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Header from './Header.jsx';
 import Home from './Home.jsx';
 import Footer from './Footer.jsx';
 
-function App() {
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to ="/">Home</Link>
+            </li>
 
-  return(
-    <>
-    <Header />
-    <Home />
-    <Footer />
-    </>
-  );
+          </ul>
+        </nav>
+
+        <switch>
+          <Route path='/' exact>
+            <Home />
+          </Route>
+        </switch>
+      </div>
+      <Footer />
+    </Router>
+  )
 }
-
-export default App
